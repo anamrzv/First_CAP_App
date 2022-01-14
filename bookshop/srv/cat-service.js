36a0ccad4842dadef2a4b32013250633fa66f272
anamrzv <ana.mrzv@mail.ru>
2022-01-14 13:14:40 +0300

    fix

--- bookshop/srv/cat-service.js
+++ bookshop/srv/cat-service.js
@@ -1,12 +1,8 @@
 const cds = require('@sap/cds') //require cds framework
 const { Books } = cds.entities
-const express = require('express');
-const app = express();
+
 const SapCfMailer = require('sap-cf-mailer').default;
-
 const transporter = new SapCfMailer("MAILTRAP");
-
-app.use(express.json());
 
 /** Service implementation for CatalogService */
 module.exports = cds.service.impl(async function () {
@@ -14,7 +10,8 @@ module.exports = cds.service.impl(async function () {
   const bupaSrv = await cds.connect.to('API_BUSINESS_PARTNER')
 
   //sending mail
-  this.on('READ', async (req) => sendMail(req))
+  //sending mail
+  this.on('READ', async (req) => sendMail(req));
 
 
   this.after('READ', 'Books', each => each.stock > 111 && _addDiscount2(each, 11))
@@ -58,7 +55,6 @@ module.exports = cds.service.impl(async function () {
     }))
   }
 
-  //has error TypeError: res.send is not a function
   async function sendMail(req) {
     console.debug('>>>>>> MAIL', req.method, req.target.name)
     const result = await transporter.sendMail({
